838c26d36fc80440daf4cf3d55030052b5bf1807
ruba1995 <ruba_1995@outlook.com>
2020-12-31 15:43:12 +0200

    problem3

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -32,8 +32,8 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-    var total =  a*b;
-
+    var total = a*b;
+    
     var returnArray = [
         total,
         "The product of 5 and 9 is 45."
@@ -61,11 +61,22 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+   
+    
+    var totalSum= sum(sum(a,b)[0],c)[0] ;
+    
+    var totalProduct = multiply(multiply(a ,b)[0],c)[0];
+    
+    var returnArray=[ 
+         totalSum ,
+         totalProduct , 
+         '4 and 7 and 5 sum to 16.' , 
+         'The product of 4 and 7 and 5 is 140.' ]
+    return returnArray ;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
